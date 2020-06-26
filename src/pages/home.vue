c4438cc4910534468f0101f1a5de040ebae02473
zdbxdong <50289347+zdbxdong@users.noreply.github.com>
2020-06-26 15:49:07 +0800

    2

---
+++ src/pages/home.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+   home
+   <Nav-header></Nav-header>
+   <route-view></route-view>
+   <Nav-footer></Nav-footer>
+ </div>
+</template>
+<script>
+import NavHeader from '../components/NavHeader'
+import NavFooter from '../components/NavFooter'
+  export default{
+    name:'nav-home',
+    components:{
+      NavFooter,
+      NavHeader
+    }
+  }
+</script>
--- src/router.js
+++ src/router.js
@@ -1,13 +1,61 @@
-import Vue from 'Vue'
+import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './components/Home'
+import Home from './pages/home'
+import Index from './pages/index'
+import Product from './pages/product'
+import Detail from './pages/detail'
+import Cart from './pages/cart'
+import Order from './pages/order'
+import OrderComfirm from './pages/orderConfirm'
+import OrderList from './pages/orderList'
+import OrderPay from './pages/orderPay'
+
 Vue.use(Router)
 export default new Router({
   routes:[
     {
       path:'/',
       name:'home',
-      component:Home
+      component:Home,
+      children:[
+        {
+          path:'/index',
+          name:'index',
+          component:Index
+        },
+        {
+          path:'/product/:id',
+          name:'product',
+          component:Product
+        },{
+          path:'/detail/:id',
+          name:'detail',
+          component:Detail
+        }
+      ]
+    },{
+      path:'/cart',
+      name:'cart',
+      component:Cart
+    },{
+      path:'/order',
+      name:'order',
+      component:Order,
+      children:[
+        {
+          path:'/list',
+          name:'order-list',
+          component:OrderList
+        },{
+          path:'/confirm',
+          name:'order-confirm',
+          component:OrderComfirm,
+        },{
+          path:'/pay',
+          name:'order-pay',
+          component:OrderPay
+        }
+      ]
     }
   ]
 
